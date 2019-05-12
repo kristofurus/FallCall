d3794ea5b3b8799641af51a5eba4c53e6808e396
Krzysztof Łada <k.lada@stud.elka.pw.edu.pl>
2019-05-12 09:26:16 +0200

    Updated code

--- hack2.ino
+++ hack2.ino
@@ -1,12 +1,19 @@
-// MPU-6050 Short Example Sketch
-// Public Domain
 #include<Wire.h>
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
+#include "AlertMe.h"
+
+//WIFI MODULE
+AlertMe alert;
+uint8_t config_pin = 12;
+String to_email = "krzysztof.ladaa@gmail.com";
+
+String subject_line = "I can't stand up! Please help!";
+String message = "I have fallen and can't stand up!\n Please come help me or call the doctor\n.";
+
+//GYROSCOPE + ACCELEROMETER
+const int MPU_addr=0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 float ax=0, ay=0, az=0, gx=0, gy=0, gz=0;
 
-//int data[STORE_SIZE][5]; //array for saving past data
-//byte currentIndex=0; //stores current data array index (0-255)
 boolean fall = false; //stores if a fall has occurred
 boolean trigger1=false; //stores if first trigger (lower threshold) has occurred
 boolean trigger2=false; //stores if second trigger (upper threshold) has occurred
@@ -17,106 +24,202 @@ byte trigger2count=0; //stores the counts past since trigger 2 was set true
 byte trigger3count=0; //stores the counts past since trigger 3 was set true
 int angleChange=0;
 
-void setup(){
+//BUZZER ALARM
+
+boolean alarm = false;
+int button_count = 0;
+
+//PINS
+
+#define BUTTON 4
+#define BUZZER 12
+
+void setup()
+{
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x6B);
+  Wire.write(0);
   Wire.endTransmission(true);
+
   Serial.begin(9600);
 
-  pinMode(12, OUTPUT);
-  //pinMode(11, OUTPUT);
-  //digitalWrite(11, HIGH);
+  pinMode(config_pin,INPUT_PULLUP);
+  if(config_pin == LOW)
+  {
+    alert.config();
+  }
+    alert.connect();
+
+  pinMode(BUZZER, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
 }
-void loop(){
-  
+void loop()
+{  
   mpu_read();
-  //2050, 77, 1947 are values for calibration of accelerometer
-  // values may be different for you
   ax = (AcX-2050)/16384.00;
   ay = (AcY-77)/16384.00;
   az = (AcZ-1947)/16384.00;
-  
-  //270, 351, 136 for gyroscope
+
   gx = (GyX+270)/131.07;
   gy = (GyY-351)/131.07;
   gz = (GyZ+136)/131.07;
-  
-  // calculating Amplitute vactor for 3 axis
-  float Raw_AM = pow(pow(ax,2)+pow(ay,2)+pow(az,2),0.5);
-  int AM = Raw_AM * 10;  // as values are within 0 to 1, I multiplied 
-                         // it by for using if else conditions 
-  
-  Serial.println(AM);
-  //Serial.println(PM);
-  //delay(500);
 
-  if (trigger3==true){
+  float Raw_AM = pow(pow(ax,2)+pow(ay,2)+pow(az,2),0.5);
+  int AM = Raw_AM * 10;
+  
+  //Serial.println(AM);
+  if (trigger3==true)
+  {
      trigger3count++;
-     //Serial.println(trigger3count);
-     if (trigger3count>=10){ 
+     if (trigger3count>=15)
+     { 
         angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5);
-        //delay(10);
-        Serial.println(angleChange); 
-        if ((angleChange>=0) && (angleChange<=15)){ //if orientation changes remains between 0-10 degrees
-            fall=true; trigger3=false; trigger3count=0;
-            Serial.println(angleChange);
-              }
-        else{ //user regained normal orientation
+        if ((angleChange>=0) && (angleChange<=15))
+        {
+          fall=true; trigger3=false; trigger3count=0;
+        }
+        else
+        {
            trigger3=false; trigger3count=0;
-           Serial.println("TRIGGER 3 DEACTIVATED");
         }
       }
    }
-  if (fall==true){ //in event of a fall detection
-    Serial.println("FALL DETECTED");
-    digitalWrite(12, HIGH);
-    delay(1000);
-    digitalWrite(12, LOW);
-    fall=false;
-   // exit(1);
-    }
-  if (trigger2count>=6){ //allow 0.5s for orientation change
-    trigger2=false; trigger2count=0;
-    Serial.println("TRIGGER 2 DECACTIVATED");
-    }
-  if (trigger1count>=6){ //allow 0.5s for AM to break upper threshold
-    trigger1=false; trigger1count=0;
-    Serial.println("TRIGGER 1 DECACTIVATED");
-    }
-  if (trigger2==true){
-    trigger2count++;
-    //angleChange=acos(((double)x*(double)bx+(double)y*(double)by+(double)z*(double)bz)/(double)AM/(double)BM);
-    angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5); Serial.println(angleChange);
-    if (angleChange>=30 && angleChange<=400){ //if orientation changes by between 80-100 degrees
-      trigger3=true; trigger2=false; trigger2count=0;
-      Serial.println(angleChange);
-      Serial.println("TRIGGER 3 ACTIVATED");
-      digitalWrite(12, HIGH);
-      delay(1000);
-      digitalWrite(12, LOW);
+  if (fall==true)
+  {
+    
+    digitalWrite(BUZZER, HIGH);
+    alarm = true;
+    for(int i = 0; i < 100; i++)
+    {
+      if(digitalRead(BUTTON)==0)
+      {
+        button_count++;
+        Serial.println(button_count);
+        delay(100);
+        if(button_count == 2)
+        {
+          digitalWrite(BUZZER, LOW);  
+          alarm = false;
+          button_count = 0;
+          break; 
         }
-    }
-  if (trigger1==true){
-    trigger1count++;
-    if (AM>=12){ //if AM breaks upper threshold (3g)
-      trigger2=true;
-      Serial.println("TRIGGER 2 ACTIVATED");
-      trigger1=false; trigger1count=0;
-      digitalWrite(12, HIGH);
-      delay(1000);
-      digitalWrite(12, LOW);
       }
+      delay(100);
     }
-  if (AM<=2 && trigger2==false){ //if AM breaks lower threshold (0.4g)
+    digitalWrite(BUZZER, LOW);
+    if(alarm == true)
+    {
+      alert.send(subject_line, message, to_email);
+      Serial.println("ALARM ALARM t3");
+    }
+    alarm = false;
+    button_count = 0;   
+    fall=false;
+  }
+  if (trigger2==true)
+  {
+    trigger2count++;
+    angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5); //Serial.println(angleChange);
+    if (angleChange>=30 && angleChange<=400)
+    {
+      trigger3=true; trigger2=false; trigger2count=0;
+      digitalWrite(BUZZER, HIGH);
+      alarm = true;
+      for(int i = 0; i < 100; i++)
+      {
+        if(digitalRead(BUTTON)==0)
+        {
+          button_count++;
+          Serial.println(button_count);
+          delay(100);
+          if(button_count == 2)
+          {
+            digitalWrite(BUZZER, LOW);  
+            alarm = false;
+            button_count = 0;
+            break; 
+          }
+        }
+        delay(100);
+      }
+      digitalWrite(BUZZER, LOW);
+       if(alarm == true)
+      {
+        alert.send(subject_line, message, to_email);
+        Serial.println("ALARM ALARM t2");
+      }
+      alarm = false;
+      button_count = 0;   
+    }
+  }
+  if (trigger1==true)
+  {
+    trigger1count++;
+    if (AM>=12)
+    {
+      trigger2=true;
+      trigger1=false; trigger1count=0;
+      digitalWrite(BUZZER, HIGH);
+      alarm = true;
+      for(int i = 0; i < 100; i++)
+      {
+        if(digitalRead(BUTTON)==0)
+        {
+          button_count++;
+          Serial.println(button_count);
+          delay(100);
+          if(button_count == 2)
+          {
+            digitalWrite(BUZZER, LOW);  
+            alarm = false;
+            button_count = 0;
+            break; 
+          }
+        }
+        delay(100);
+      }
+      digitalWrite(BUZZER, LOW);
+      if(alarm == true)
+      {
+        alert.send(subject_line, message, to_email);
+        Serial.println("ALARM ALARM t1");
+      }
+      alarm = false;
+      button_count = 0;   
+    }
+  }
+  if (AM<=2 && trigger2==false)
+  {
     trigger1=true;
-    Serial.println("TRIGGER 1 ACTIVATED");
-    digitalWrite(12, HIGH);
-    delay(1000);
-    digitalWrite(12, LOW);
+    digitalWrite(BUZZER, HIGH);
+    alarm = true;
+    for(int i = 0; i < 100; i++)
+    {
+      if(digitalRead(BUTTON)==0)
+      {
+        button_count++;
+        Serial.println(button_count);
+        delay(100);
+        if(button_count == 2)
+        {
+          digitalWrite(BUZZER, LOW);  
+          alarm = false;
+          button_count = 0;
+          break; 
+        }
+      }
+      delay(100);
     }
-//It appears that delay is needed in order not to clog the port
+    digitalWrite(BUZZER, LOW);
+    if(alarm == true)
+      {
+        alert.send(subject_line, message, to_email);
+        Serial.println("ALARM ALARM unknown");
+      }
+    alarm = false;
+    button_count = 0;   
+  }
   delay(100);
 }
 
